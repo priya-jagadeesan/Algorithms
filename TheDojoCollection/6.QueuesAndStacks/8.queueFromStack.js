97528fcd7ebc522187f5fb4642db0618bd303b23
Priya <priyajagadeesan88@gmail.com>
2018-04-07 21:43:25 -0700

    modified queueFromStack.js

--- TheDojoCollection/6.QueuesAndStacks/8.queueFromStack.js
+++ TheDojoCollection/6.QueuesAndStacks/8.queueFromStack.js
@@ -1,61 +1,126 @@
 var arrStack = require('./_ArrStack');
 var listStack = require('./_SLStack');
-var queue = require('./_SLQueue');
 
-function queueFromStack(stack){
-    var newQ = new queue();
+function queue(){
+    var fwd_stack = new arrStack(); // for enqueue()
+    var rev_stack = new arrStack(); // for dequeue()
 
-    if(!stack.size()){
-        console.log("empty stack");
-        return newQ;
+    this.enqueue = function(val){
+        fwd_stack.push(val);
+        return true;
     }
-    var tempS = new arrStack();
-    var elem;
 
-    while(stack.size()){
-        tempS.push(stack.pop());
+    this.dequeue = function(){
+        if(!fwd_stack.isEmpty()){
+            while(fwd_stack.size()){
+                rev_stack.push(fwd_stack.pop());
+            }
+            var val = rev_stack.pop();
+            while(rev_stack.size()){
+                fwd_stack.push(rev_stack.pop());
+            }
+            return val;
+        }
+        return null;
     }
-    while(tempS.size()){
-        elem = tempS.pop();
-        newQ.enqueue(elem);
-        stack.push(elem);
+
+    this.front = function(){
+        if(!fwd_stack.isEmpty()){
+            while(fwd_stack.size()){
+                rev_stack.push(fwd_stack.pop());
+            }
+            var val = rev_stack.top();
+            while(rev_stack.size()){
+                fwd_stack.push(rev_stack.pop());
+            }
+            return val;
+        }
+        return null;
     }
-    return newQ;
+
+    this.contains = function(val){
+        if(!fwd_stack.isEmpty()){
+            var flag = false;
+            while(fwd_stack.size()){
+                if(fwd_stack.top() === val){
+                    flag = true;
+                    break;
+                }
+                rev_stack.push(fwd_stack.pop());
+            }
+
+            while(rev_stack.size()){
+                fwd_stack.push(rev_stack.pop());
+            }
+            return flag;
+        }
+        return false;
+    }
+
+    this.isEmpty = function(){
+        return fwd_stack.isEmpty();
+    }
+
+    this.SLQlength = function(){
+        var count = 0;
+        if(!fwd_stack.isEmpty()){
+            while(fwd_stack.size()){
+                rev_stack.push(fwd_stack.pop());
+            }
+
+            while(rev_stack.size()){
+                count++;
+                fwd_stack.push(rev_stack.pop());
+            }
+        }
+        return count;
+    }
+
+    this.SLQdisplay = function(){
+        var str = '';
+        if(!fwd_stack.isEmpty()){
+            while(fwd_stack.size()){
+                rev_stack.push(fwd_stack.pop());
+            }
+
+            while(rev_stack.size()){
+                str += rev_stack.top() + ',';
+                fwd_stack.push(rev_stack.pop());
+            }
+        }
+        return str;
+    }
 }
 
-console.log("-----------------Array Stack-------------------------");
-var arrStack1 = new arrStack();
-console.log("stack1 : ", arrStack1.SLSdisplay());
-console.log("Create queue from empty stack: ", queueFromStack(arrStack1).SLQdisplay());
-console.log("After creating : stack : ", arrStack1.SLSdisplay());
-console.log("-----------------------------------------------------");
-arrStack1.push(10);
-console.log("stack : ", arrStack1.SLSdisplay());
-console.log("Create queue from a stack only head: ", queueFromStack(arrStack1).SLQdisplay());
-console.log("After creating : stack : ", arrStack1.SLSdisplay());
-console.log("-----------------------------------------------------");
-arrStack1.push(20);
-arrStack1.push(30);
-arrStack1.push(40);
-console.log("stack : ", arrStack1.SLSdisplay());
-console.log("Create queue from a stack: ", queueFromStack(arrStack1).SLQdisplay());
-console.log("After creating : stack : ", arrStack1.SLSdisplay());
-console.log("-----------------------------------------------------");
-console.log("------------------List Stack-------------------------");
-var listStack1 = new listStack();
-console.log("stack1 : ", listStack1.SLSdisplay());
-console.log("Create queue from a empty stack: ", queueFromStack(listStack1).SLQdisplay());
-console.log("After creating : stack : ", listStack1.SLSdisplay());
-console.log("-----------------------------------------------------");
-listStack1.push(10);
-console.log("stack : ", listStack1.SLSdisplay());
-console.log("Create queue from a stack only head: ", queueFromStack(listStack1).SLQdisplay());
-console.log("After creating : stack : ", listStack1.SLSdisplay());
-console.log("-----------------------------------------------------");
-listStack1.push(20);
-listStack1.push(30);
-listStack1.push(40);
-console.log("stack : ", listStack1.SLSdisplay());
-console.log("Create queue from a stack: ", queueFromStack(listStack1).SLQdisplay());
-console.log("After creating : stack : ", listStack1.SLSdisplay());
-console.log("-----------------------------------------------------");
+// create an empty queue
+var queue1 = new queue();
+console.log("Queue empty? :" , queue1.isEmpty());
+console.log("Empty Queue size : " , queue1.SLQlength());
+console.log("Queue contains val? :" , queue1.contains(10));
+console.log("Empty Queue front value : " , queue1.front());
+console.log("Dequeue empty queue : " , queue1.dequeue());
+console.log("Empty Queue values: " , queue1.SLQdisplay());
+console.log("-------------------------------------------------");
+console.log("Queue enqueue: " ,queue1.enqueue(10));
+console.log("Queue values: " , queue1.SLQdisplay());
+console.log("Queue empty? : " , queue1.isEmpty());
+console.log("Queue size? : " , queue1.SLQlength());
+console.log("Queue contains value 10? : " , queue1.contains(10));
+console.log("Queue front value? : " , queue1.front());
+console.log("Dequeue : " , queue1.dequeue());
+console.log("Queue values: " , queue1.SLQdisplay());
+console.log("-------------------------------------------------");
+
+queue1.enqueue(10);
+queue1.enqueue(20);
+queue1.enqueue(30);
+queue1.enqueue(40);
+queue1.enqueue(50);
+queue1.enqueue(60);
+console.log("Queue values: " , queue1.SLQdisplay());
+console.log("Queue size? : " , queue1.SLQlength());
+console.log("Queue front value? : " , queue1.front());
+console.log("Dequeue front : " , queue1.dequeue());
+console.log("Queue size? : " , queue1.SLQlength());
+console.log("-------------------------------------------------");
+console.log("Queue values: " , queue1.SLQdisplay());
